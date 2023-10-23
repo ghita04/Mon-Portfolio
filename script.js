6061c8a03eb979f4c2d1d8376104b5b9d5fbdfb3
ghita04 <alaouighita58@gmail.com>
2023-10-22 20:49:14 -0400

    ajout-swiper_correction

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const swiper = new Swiper(".mySwiper", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperMontage = new Swiper(".swipermontage", {
+  direction: "horizontal",
+  slidesPerView: 1,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
